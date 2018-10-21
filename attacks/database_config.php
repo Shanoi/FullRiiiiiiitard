928e10d7484cf83a32c064a0fea3c88c0b80093e
Shanoi <olivboulet@laposte.net>
2018-10-21 18:17:47 +0200

    Maj config BD

--- attacks/database_config.php
+++ attacks/database_config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost:3307');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
+   define('DB_USERNAME', 'user');
+   define('DB_PASSWORD', 'SAWLeJeudiMatin');
    define('DB_DATABASE', 'saw');
    $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 ?>

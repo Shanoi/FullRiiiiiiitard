37fb00fe3effa852541ed67b23493aa7088239cd
Jérémy Lara <jeremy.lara.pro@gmail.com>
2018-10-21 20:51:58 +0200

    secret service implemented

--- attacks/welcome.php
+++ attacks/welcome.php
@@ -10,12 +10,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $pwd = $_POST["pwd"];
 
-    $result = $db->query("SELECT username, pwd FROM users WHERE username='$username'");
+    $result = $db->query("SELECT username, pwd, `admin` FROM users WHERE username='$username'");
 
     $result->setFetchMode(PDO::FETCH_BOTH);
 
     $row = $result->fetch();
-
+    
+    // Debug only
+    //echo print_r($row);
+    
     if (!$row) {
         $error = "Your Username is invalid";
     } else {
@@ -26,6 +29,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             setcookie("session_username", $username, 2147483647);
 
             echo "<h1>Welcome " . $username . "</h1>";
+
+            if ($row['admin'] == 1) {
+                echo "<div id=secret><a href=\"secret_service.php\">/!\ Secret Service /!\</a></div></br>";
+            }
+
             echo "<div><a href=\"logout.php\">Logout</a></div>";
 
         } else {

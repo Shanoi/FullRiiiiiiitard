485f6a86582559a4fcea7b70e5aa36f7ae3c08f7
Shanoi <olivboulet@laposte.net>
2018-10-21 18:23:55 +0200

    maj init BD

--- attacks/init_db.sql
+++ attacks/init_db.sql
@@ -1,22 +1,16 @@
-
 DROP TABLE IF EXISTS saw.users;
-
-CREATE TABLE IF NOT EXISTS saw.users (
-  id int(10) NOT NULL,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 DROP TABLE IF EXISTS saw.admins;
 
-CREATE TABLE IF NOT EXISTS saw.admins (
-  id int(10) NOT NULL,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS saw.users (
+  id           INT(10)     NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name`  VARCHAR(45) NOT NULL,
+  `admin`      BOOLEAN     NOT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
-INSERT IGNORE INTO admins (id, `first_name`, `last_name`) VALUES (0, "Alice", "Manager");
-INSERT IGNORE INTO users (id, `first_name`, `last_name`) VALUES (0, "Bob", "Random");
+
+INSERT IGNORE INTO users (id, `first_name`, `last_name`, `admin`) VALUES (0, "Alice", "Manager", 1);
+INSERT IGNORE INTO users (id, `first_name`, `last_name`, `admin`) VALUES (0, "Bob", "Random", 0);

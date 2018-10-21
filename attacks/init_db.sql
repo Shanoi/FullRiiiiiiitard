b543305a122711bb7df53d676ad8c9052a12627d
Shanoi <olivboulet@laposte.net>
2018-10-21 17:58:45 +0200

    db init

--- attacks/init_db.sql
+++ attacks/init_db.sql
@@ -1,12 +1,22 @@
-/*
+
 DROP TABLE IF EXISTS saw.users;
-*/
 
 CREATE TABLE IF NOT EXISTS saw.users (
-	id int(10) NOT NULL,
-	`name` varchar(45) NOT NULL,
-	PRIMARY KEY (id)
+  id int(10) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT IGNORE INTO users (id, `first_name`, `last_name`) VALUES (0, "Alice", "Manager");
-INSERT IGNORE INTO users (id, `first_name`, `last_name`) VALUES (1, "Bob", "Random");
+
+DROP TABLE IF EXISTS saw.admins;
+
+CREATE TABLE IF NOT EXISTS saw.admins (
+  id int(10) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT IGNORE INTO admins (id, `first_name`, `last_name`) VALUES (0, "Alice", "Manager");
+INSERT IGNORE INTO users (id, `first_name`, `last_name`) VALUES (0, "Bob", "Random");

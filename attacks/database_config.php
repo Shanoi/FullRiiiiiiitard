1ec5d4a9e8638a264838ba2bef972ac1bc4e13fc
Jérémy Lara <jeremy.lara.pro@gmail.com>
2018-10-21 17:49:38 +0200

    changed database_config

--- attacks/database_config.php
+++ attacks/database_config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', 'localhost:3307');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root');
+   define('DB_PASSWORD', '');
    define('DB_DATABASE', 'saw');
    $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
 ?>
--- attacks/index.php
+++ attacks/index.php
@@ -1,15 +1,14 @@
 <?php
 include("database_config.php");
 
+$error = "";
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
 
     /*$myPDO = new PDO('mysql:host=localhost;dbname=saw', 'root', 'root');*/
-    $result = $myPDO->query("SELECT first_name, last_name FROM users WHERE first_name='$first_name' AND last_name='$last_name'");
-    if (!$result->fetch()) {
-
-    }
+    $result = $db->query("SELECT first_name, last_name FROM users WHERE first_name='$first_name' AND last_name='$last_name'");
     
     if(!$result->fetch()) {
         $error = "Your Login Name or Password is invalid";
@@ -25,7 +24,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h1>Login</h1>
 
-<form action="login.php" method="POST">
+<form action="welcome.php" method="POST">
 First name: <input type="text" name="first_name" value="Mr"><br>
 Last name: <input type="text" name="last_name" value="Robot"><br>
 <input type="submit" value="Login">
